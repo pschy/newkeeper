2f3ef7f1e4fda7e768b66ca3172fbff0d3805d21
wangyifan <wangyifan@diynova.com>
2022-09-02 20:05:21 +0800

    uwsgi debug

--- newkeeper/newkeeper/urls.py
+++ newkeeper/newkeeper/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, re_path
 from django.views.static import serve
 from django.conf import settings
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newkeeper.settings")
+django.setup()
 
 urlpatterns = [
     re_path(r'^api/v(?P<version>[0-9]+)/', include("api.urls")),
